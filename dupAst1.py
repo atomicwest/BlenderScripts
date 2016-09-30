9b053bdf58257c688486dac280cd3f24510cbcaa
Jesson Go <ogjdne@gmail.com>
2016-09-30 06:37:17 +0000

    add asteroid duplication

---
+++ dupAst1.py
@@ -0,0 +1,21 @@
+import bpy
+import random
+import math
+
+orig = bpy.context.object.data
+dupTemp = orig.copy()
+
+for i in range(15):
+    num = "%04d"%i
+    newName = "Asteroid%s" %num
+    dup = bpy.data.objects.new("Asteroid_new", dupTemp)
+    
+    newX = math.ceil(random.random()*20)
+    newY = math.ceil(random.random()*20)
+    newZ = math.ceil(random.random()*20)
+    
+    dup.location = (newX,newY,newZ)
+
+    scene = bpy.context.scene
+    scene.objects.link(dup)
+    scene.update()
