c3e0f9eaeba170e35138d6f06b05e080db186736
Jesson Go <ogjdne@gmail.com>
2016-10-11 10:54:51 +0000

    edit duplication scripts

--- dupObj.py
+++ dupObj.py
@@ -2,21 +2,22 @@ import bpy
 import random
 import math
 
+obj = bpy.context.object
 orig = bpy.context.object.data
 dupTemp = orig.copy()
 
 for i in range(15):
     num = "%04d"%i
-    newName = "Asteroid%s" %num
+    newName = "%s%s" %(obj.name,num)
     dup = bpy.data.objects.new(newName, dupTemp)
     
-    newX = math.ceil(random.uniform(-1,1)*20)
-    newY = math.ceil(random.uniform(-1,1)*20)
-    newZ = math.ceil(random.uniform(-1,1)*20)
+    newX = math.ceil(random.uniform(-1,1)*25 + i)
+    newY = math.ceil(random.uniform(-1,1)*25 + i)
+    newZ = math.ceil(random.uniform(-1,1)*25 + i)
     
     dup.location = (newX,newY,newZ)
     dup.rotation_euler = (newX, newY, newZ)
-    randScale = [-3,-2,-1,1,2,3]
+    randScale = [-3,-2,-4,4,2,3]
     dup.scale = (random.choice(randScale), random.choice(randScale), random.choice(randScale))
     
     
