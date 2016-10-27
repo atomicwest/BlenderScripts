f30416f57711ab45be54d5df0fd26d9a26681971
Jesson Go <ogjdne@gmail.com>
2016-10-27 11:53:37 +0000

    edit hyperspace scaling for location function

--- hyperspace_scale.py
+++ hyperspace_scale.py
@@ -1,8 +1,14 @@
-#nonparticle method for hyperspace entry effect
-#create an icosphere and name it "star" something
-#make n many duplicates of the star object
-#this script will scale all of the stars uniformly
-#across however many frames when setframe is called
+'''
+nonparticle method for hyperspace entry effect
+create an icosphere and name it "star" something
+make n many duplicates of the star object.
+
+The setloc function will keyframe the location of each
+matching object
+
+The setsize function scale all of the stars uniformly
+across however many frames when setframe is called
+'''
 
 import bpy
 import math
@@ -16,7 +22,7 @@ scn.frame_end = 1200
 all = bpy.data.objects
 
 
-def setframe(frame, obj, grow):
+def setsize(frame, obj, grow):
     scal = obj.scale
     print(scal[0])
     bpy.context.scene.frame_set(frame)
@@ -25,6 +31,13 @@ def setframe(frame, obj, grow):
     
     bpy.ops.anim.keyframe_insert_menu(type='LocRotScale')
     
+def setloc(frame, obj, dist):
+    #hardcoded for y-axis
+    loc = obj.location
+    print(loc[1])
+    bpy.context.scene.frame_set(frame)
+    obj.location[1] = obj.location[1] + dist
+    bpy.ops.anim.keyframe_insert_menu(type='LocRotScale')
 
 for obj in all:
 
@@ -35,5 +48,16 @@ for obj in all:
         continue
 
     print(obj.name)
-    setframe(30,obj, 1)
-    setframe(90,obj, 200)
+    
+    #set move objects 10000 units away from current position
+    #and key at frame 150
+    setloc(150,obj, 10000) 
+    
+    #return objects to original position at frame 180
+    setloc(180,obj, 0)
+
+    #starting point at frame 30
+    setsize(30,obj, 1)
+    
+    #stretch by 200 units at frame 90
+    setsize(90,obj, 200)
