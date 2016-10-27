de14cde04f8ad93d3acc4bfd23f97c4e7c4774bf
Jesson Go <ogjdne@gmail.com>
2016-10-27 13:35:14 +0000

    update asteroid_animate.py with rotation function

--- asteroid_animate.py
+++ asteroid_animate.py
@@ -1,3 +1,16 @@
+'''
+More generalized LocRotScale script
+
+The setloc function will keyframe the location of each
+matching object
+
+The setsize function scale all of the stars uniformly
+across however many frames when setframe is called
+
+Setrot will keyframe rotations on each axis with a 
+range of magnitudes based on the third argument passed
+'''
+
 import bpy
 import math
 import random
@@ -10,26 +23,48 @@ scn.frame_end = 1200
 #all = bpy.context.scene.objects
 all = bpy.data.objects
 
+target = "Asteroid"
 
-def setframe(frame, rot, dist):
-    sRot = random.choice([-1,1])*rot
+def setsize(frame, obj, grow):
+    scal = obj.scale
+    print(scal[0])
     bpy.context.scene.frame_set(frame)
-    # bpy.ops.transform.rotate(value=(sRot*math.pi ), axis=(random.choice([-1,1]), random.choice([-1,1]), random.choice([-1,1])))
-    bpy.ops.transform.rotate(value=(sRot*math.pi ), axis=(0, 0, 0))
-    # bpy.ops.transform.translate(value=(random.uniform(-dist,dist), random.uniform(-dist,dist),random.uniform(-dist,dist)))
-    bpy.ops.transform.translate(value=(0, 0,0))
+    
+    obj.scale[1] = obj.scale[1]*grow
+    
+    bpy.ops.anim.keyframe_insert_menu(type='LocRotScale')
+    
+def setloc(frame, obj, dist):
+    #hardcoded for y-axis
+    loc = obj.location
+    print(loc[1])
+    bpy.context.scene.frame_set(frame)
+    obj.location[1] = obj.location[1] + dist
+    bpy.ops.anim.keyframe_insert_menu(type='LocRotScale')
 
-    # create keyframe
-    bpy.ops.anim.keyframe_insert_menu(type='Rotation')
-    bpy.ops.anim.keyframe_insert_menu(type='Location')
+def setrot(frame, obj, magnitude):
+    #hardcoded for y-axis
+    rot = obj.rotation_euler
+    print(rot)
+    bpy.context.scene.frame_set(frame)
+    
+    rotX = obj.rotation_euler[0] + random.uniform(-1,1)*random.uniform(0,magnitude)
+    rotY = obj.rotation_euler[0] + random.uniform(-1,1)*random.uniform(0,magnitude)
+    rotZ = obj.rotation_euler[0] + random.uniform(-1,1)*random.uniform(0,magnitude)
+    
+    obj.rotation_euler = (rotX, rotY, rotZ)
+    bpy.ops.anim.keyframe_insert_menu(type='LocRotScale')
+
 
 for obj in all:
 
-    if ("Asteroid" in obj.name):
+    if (target in obj.name):
         bpy.data.objects[obj.name].select=True
     else:
         bpy.data.objects[obj.name].select=False
         continue
 
-    setframe(1,0.005,1)
-    setframe(1200,0.03,5)
+    print(obj.name)
+    #setloc(150,obj, 10000)
+    setrot(0,obj, 0)
+    setrot(250, obj, 45)
