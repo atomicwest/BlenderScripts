352a7abb948c3d060e870473b98c4349ecd00ee2
Jesson Go <ogjdne@gmail.com>
2017-03-13 08:24:43 +0000

    edit swap L-R script

--- MassMakerToolbox/standalone/swap_L-R_name.py
+++ MassMakerToolbox/standalone/swap_L-R_name.py
@@ -1,33 +1,45 @@
+'''
+select an mesh object that has children, and swap the naming convention
+to the opposite side; e.g. armor_leg_L swap to armor_leg_R
+can be edited to cover multiple naming schemes besides "_L"
+'''
+
 import bpy
 
-kids = bpy.context.scene.objects.active.children
+print(bpy.context.scene.objects.active.name)
 
+allobjects = bpy.data.objects
+
+print(bpy.context.scene.objects.active.children)
+
+selected = bpy.context.scene.objects.active
+
+kids = selected.children
+
+
+def swap(meshobj):
+    if ("_L." in meshobj.name) | ("_L_" in meshobj.name):
+        try:
+            start = meshobj.name.index("_L.")
+        except:
+            start = meshobj.name.index("_L_")
+                
+        new = meshobj.name[0:start+1]
+        new+="R" 
+    else:
+        try:
+            start = meshobj.name.index("_R.")
+        except:
+            start = meshobj.name.index("_R_")
+                
+        new = meshobj.name[0:start+1]
+        new+="L" 
+           
+        meshobj.name=new
+
+
+swap(selected)      #swap naming scheme of the parent object
+
+#swap naming scheme of children objects
 for child in kids:
-    new = ""
-    if ("_L." in child.name) | ("_L_" in child.name):
-        try:
-            start = child.name.index("_L.")
-        except:
-            start = child.name.index("_L_")
-
-        for i in range(len(child.name)):
-            if i == start+1:
-                new+="R"
-            else:
-                new+=child.name[i]
-
-        child.name=new
-
-    if ("_R." in child.name) | ("_R_" in child.name):
-        try:
-            start = child.name.index("_R.")
-        except:
-            start = child.name.index("_R_")
-
-        for i in range(len(child.name)):
-            if i == start+1:
-                new+="L"
-            else:
-                new+=child.name[i]
-
-        child.name=new
+    swap(child)
